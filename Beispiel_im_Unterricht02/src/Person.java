8e04aaf26b6dc0a846491a0a262f99264a2e6942
Nimit Singh <nimit.singh20@yahoo.com>
2018-10-23 14:44:53 +0200

    Namen Vergleich

---
+++ Beispiel_im_Unterricht02/src/Person.java
@@ -0,0 +1,87 @@
+
+public class Person {
+	private String name;
+	private String firstname;
+	int age;
+	
+	public Person(String name, String firstname, int age) {
+		super();
+		this.name = name;
+		this.firstname = firstname;
+		this.age = age;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public boolean isBiggerthan(Person p)
+	{
+		int slength;
+		
+		if(this.name.length() < p.getName().length())
+		{
+			slength = this.name.length();
+		}
+		else
+		{
+			slength = p.getName().length();
+		}
+		
+		for(int i = 0; i < slength; i++)
+		{
+			if(this.name.charAt(i) < p.getName().charAt(i))
+			{
+				return true;
+			}
+			else if(this.name.charAt(i) > p.getName().charAt(i))
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public boolean isSmallerthan(Person p)
+	{
+		int slength;
+		
+		if(this.name.length() < p.getName().length())
+		{
+			slength = this.name.length();
+		}
+		else
+		{
+			slength = p.getName().length();
+		}
+		
+		for(int i = 0; i < slength; i++)
+		{
+			if(this.name.charAt(i) > p.getName().charAt(i))
+			{
+				return true;
+			}
+			else if(this.name.charAt(i) < p.getName().charAt(i))
+			{
+				return false;
+			}
+		}
+		
+		return true;
+	}
+}
