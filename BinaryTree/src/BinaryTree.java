88a4d3d5679820c7623484d601958408de5f7ad4
Nimit Singh <nimit.singh20@yahoo.com>
2018-12-06 08:34:46 +0100

    BinaryTree

--- BinaryTree/src/BinaryTree.java
+++ BinaryTree/src/BinaryTree.java
@@ -28,5 +28,39 @@ public class BinaryTree {
 	{
 		root = head(root, value);
 	}
+	
+	private boolean containsNodeRecursive(Node current, int value) {
+	    if (current == null) {
+	        return false;
+	    } 
+	    if (value == current.value) {
+	        return true;
+	    } 
+	    return value < current.value
+	      ? containsNodeRecursive(current.left, value)
+	      : containsNodeRecursive(current.right, value);
+	}
+	
+	public boolean containsNode(int value) {
+	    return containsNodeRecursive(root, value);
+	}
+	
+	public void givenABinaryTree_WhenAddingElements_ThenTreeContainsThoseElements() {
+	    BinaryTree bt = createBinaryTree();
+	 
+	    assertTrue(bt.containsNode(6));
+	    assertTrue(bt.containsNode(4));
+	  
+	    assertFalse(bt.containsNode(1));
+	}
+
+	private void assertFalse(boolean containsNode){}
+
+	private void assertTrue(boolean containsNode){}
+
+	private BinaryTree createBinaryTree() {
 		
+		return null;
+	}
+	
 }
