783c38c666458a1012d4cb992d109e1b271d6ec7
Nimit Singh <nimit.singh20@yahoo.com>
2019-01-17 08:56:07 +0100

    Bubble_Sort

---
+++ Bubble_Sort/src/Programm_2.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Programm_2 {
+	
+	public static void main(String []args)
+	{
+		long time = System.currentTimeMillis();
+		
+		ArrayList Liste = new ArrayList<Integer>();
+		Liste.add(23);
+		Liste.add(10);
+		Liste.add(23);
+		Liste.add(23);
+		Liste.add(23);
+		Liste.add(23);
+		Liste.add(23);
+		System.out.println("");
+	}
+}
